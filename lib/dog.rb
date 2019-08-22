1387ee8e5270d9f9e72691dc5ca471a3c88ebbdc
Brittany Javalera <brittjavalera@gmail.com>
2019-08-22 03:19:42 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -35,8 +35,8 @@ attr_accessor :name, :breed, :id
     SQL
     DB[:conn].execute(sql, self.name, self.breed)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-    self
   end
+  self
   end
   
   def self.create(name:, breed:)
@@ -63,13 +63,14 @@ attr_accessor :name, :breed, :id
       dog_data = dog[0]
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else
-      song = self.create(name: name, breed: breed)
+      dog = self.create(name: name, breed: breed)
     end
+    dog
   end
     
   def self.find_by_name(name)
     sql = <<-SQL
-      SELECT * FROM dogs WHERE name = ?
+      SELECT * FROM dogs WHERE name = ? LIMIT 1
     SQL
 
     DB[:conn].execute(sql, name).map do |row|
