51a42a771c39e7b218a9b94c3b900fbc49e7e3cd
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-29 05:28:39 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -41,7 +41,7 @@ class Dog
     sql = <<-SQL
       SELECT * FROM dogs WHERE name = ? LIMIT 1
     SQL
-    DB[:conn].execute(sql,name).map do |row|
+    DB[:conn].execute(sql, name).collect do |row|
       self.new_from_db(row)
     end.first
   end
